e9bddb3a6d2671daa9ca5339b2a2cc61a9849262
jose vitor <josevitorrodrigues17@gmail.com>
2025-05-28 18:42:19 -0300

    feat: func to verify credentials for user to sign in

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -2,15 +2,19 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/josevitorrodriguess/gochat/internal/errors"
 	"github.com/josevitorrodriguess/gochat/internal/models"
+	"github.com/josevitorrodriguess/gochat/internal/utils/crypt"
 )
 
 type UserRepository interface {
 	CreateUser(ctx context.Context, user models.User) error
+	AuthenticateUser(ctx context.Context, email, pass string) (uuid.UUID, error)
 }
 
 type userRepository struct {
@@ -35,4 +39,23 @@ func (ur *userRepository) CreateUser(ctx context.Context, user models.User) erro
 	}
 
 	return nil
+}
+
+func (ur *userRepository) AuthenticateUser(ctx context.Context, email, pass string) (uuid.UUID, error) {
+	query := `SELECT id, username, email, password FROM users WHERE email = $1`
+	var user models.User
+
+	err := ur.db.GetContext(ctx, &user, query, email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return uuid.Nil, errors.NewNotFoundError("user not found", err)
+		}
+		return uuid.Nil, errors.NewInternalError("database error", err)
+	}
+	ok := crypt.CheckPasswordHash(pass, user.Password)
+	if !ok {
+		return uuid.Nil, errors.NewUnauthorizedError("invalid credentials", err)
+	}
+
+	return user.ID, nil
 }
