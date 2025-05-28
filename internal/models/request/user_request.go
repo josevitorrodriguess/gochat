e0a434e4114f531c5d440c905088cedba00669c6
jose vitor <josevitorrodrigues17@gmail.com>
2025-05-28 18:41:23 -0300

    feat: signin model

--- internal/models/request/user_request.go
+++ internal/models/request/user_request.go
@@ -25,7 +25,6 @@ func (ur *UserRequest) IsValid() (bool, error) {
 		return false, errors.New("username must be at most 20 characters")
 	}
 
-	
 	if strings.TrimSpace(ur.Email) == "" {
 		return false, errors.New("email is required")
 	}
@@ -33,7 +32,6 @@ func (ur *UserRequest) IsValid() (bool, error) {
 		return false, errors.New("invalid email format")
 	}
 
-	
 	if strings.TrimSpace(ur.Password) == "" {
 		return false, errors.New("password is required")
 	}
@@ -48,16 +46,42 @@ func (ur *UserRequest) IsValid() (bool, error) {
 }
 
 func (ur *UserRequest) ToUserModel() models.User {
+
 	return models.User{
 		Username: strings.TrimSpace(ur.Username),
 		Email:    strings.TrimSpace(ur.Email),
-		Password: ur.Password, 
+		Password: ur.Password,
 	}
 }
-
 
 func isValidEmail(email string) bool {
 	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
 	re := regexp.MustCompile(emailRegex)
 	return re.MatchString(email)
+}
+
+type SignInRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (sir *SignInRequest) IsValid() (bool, error) {
+	if strings.TrimSpace(sir.Email) == "" {
+		return false, errors.New("email is required")
+	}
+	if !isValidEmail(sir.Email) {
+		return false, errors.New("invalid email format")
+	}
+
+	if strings.TrimSpace(sir.Password) == "" {
+		return false, errors.New("password is required")
+	}
+	if len(sir.Password) < 6 {
+		return false, errors.New("password must be at least 6 characters")
+	}
+	if len(sir.Password) > 20 {
+		return false, errors.New("password must be at most 20 characters")
+	}
+
+	return true, nil
 }
